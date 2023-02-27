b68be0d7d1bf2fa29936f77a6cf4147c7205691b
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2023-02-27 06:48:19 -0300

    controller cpu

--- controller.v
+++ controller.v
@@ -2,16 +2,16 @@ module controller#(localparam Width = 3)(input rst,
                                                zero,
                                                Phase,
                                          input wire [Width:0] Opcode,
-                                         input clk,
-                                        output sel,
-                                               rd,
-                                               ld_ir,
-                                               halt,
-                                               inc_pc,
-                                               ld_ac,
-                                               wr,
-                                               ld_pc,
-                                               data_e
+                                         input      clk,
+                                         output reg sel,
+                                                    rd,
+                                                    ld_ir,
+                                                    halt,
+                                                    inc_pc,
+                                                    ld_ac,
+                                                    wr,
+                                                    ld_pc,
+                                                    data_e
 );
         parameter [3:0] INST_ADDR  = 4'h0,
                         INST_FETCH = 4'h1,
@@ -22,16 +22,32 @@ module controller#(localparam Width = 3)(input rst,
                         ALU_OP     = 4'h7,
                         STORE      = 4'h8;
         reg [3:0] current_state, next_state;
-        always@(negedge clk,posedge rst)begin
-            casez({sel,rd,ld_ir,halt,inc_pc,ld_ac,ld_pc,wr,data_e})
-                 9'b100000000: next_state = INST_FETCH;
-                 9'b110000000: next_state = INST_LOAD ;
-                 9'b111000000: next_state = IDLE      ;
-                 9'b000x10000: next_state = OP_FETCH  ;
-                 9'b0x0010000: next_state = ALU_OP    ;
-
-
-
+        always@(*)begin
+            case(current_state)
+                 INST_ADDR : next_state = rst  ? 0 : INST_FETCH;
+                 INST_FETCH: next_state = INST_LOAD            ;
+                 INST_LOAD : next_state = IDLE                 ;
+                 IDLE      : next_state = OP_ADDR              ;
+                 OP_ADDR   : next_state = OP_FETCH             ;
+                 OP_FETCH  : next_state = ALU_OP               ;
+                 ALU_OP    : next_state = STORE                ;
+                 STORE     : next_state = INST_ADDR            ;
+            endcase
+        end
+        always@(posedge clk)begin
+            current_state <= rst ? 0 : next_state;
+        end
+        
+        always@(*)begin
+            case(current_state)
+                INST_ADDR :{sel,rd,ld_ir,halt,inc_pc,ld_ac,ld_pc,wr,data_e} = 9'b1_0000_0000;
+                INST_FETCH:{sel,rd,ld_ir,halt,inc_pc,ld_ac,ld_pc,wr,data_e} = 9'b1_1000_0000;
+                INST_LOAD :{sel,rd,ld_ir,halt,inc_pc,ld_ac,ld_pc,wr,data_e} = 9'b1_1100_0000;
+                IDLE      :{sel,rd,ld_ir,halt,inc_pc,ld_ac,ld_pc,wr,data_e} = 9'b1_1100_0000;
+                OP_ADDR   :{sel,rd,ld_ir,halt,inc_pc,ld_ac,ld_pc,wr,data_e} = 9'b0_00z1_0000;
+                OP_FETCH  :{sel,rd,ld_ir,halt,inc_pc,ld_ac,ld_pc,wr,data_e} = 9'b0_z000_000z;
+                ALU_OP    :{sel,rd,ld_ir,halt,inc_pc,ld_ac,ld_pc,wr,data_e} = 9'b0_z000_0000;
+                STORE     :{sel,rd,ld_ir,halt,inc_pc,ld_ac,ld_pc,wr,data_e} = 9'b0_z000_0000;
             endcase
         end
 endmodule
